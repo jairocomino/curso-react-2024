ce3099896e905475b87c97fafd3d732df2e33e3e
jairocomino <jairocominoruiz@gmail.com>
2024-01-18 13:25:46 +0100

    card and modal arreglados

--- proyectos/00-introduccion/src/components/UseEffect/GitHubUserCardApi/CardGitHub.jsx
+++ proyectos/00-introduccion/src/components/UseEffect/GitHubUserCardApi/CardGitHub.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 
 const CardGitHub = (props) => {
-  const { key, avatar_url, login, html_url}= props
+  const { key, avatar_url, login, html_url,openModal,closeModal}= props
+  function handleClickimg() {
+    openModal(avatar_url)
+  }
   return (
     <div className=' relative flex w-80 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md my-5'>
       <div key={key} className='relative mx-4 mt-6 h-40 overflow-visible rounded-xl bg-blue-500 bg-clip-border text-white shadow-lg
       shadow-blue-500/40 bg-gradient-to-t from-blue-500 to-blue-900
       '>
-        <img src={avatar_url} alt='' className='w-32 h-32 rounded-full my-4 mx-auto cursor-pointer'/>
+        <img src={avatar_url} onClick={handleClickimg} alt='' className='w-32 h-32 rounded-full my-4 mx-auto cursor-pointer'/>
       </div>
       <div className='p-6'>  
         <h5 className='mb-2 block font-sans text-xl font-semibold leading-snug -tracking-normal text-blue-900 antialiased'>
@@ -19,7 +22,7 @@ const CardGitHub = (props) => {
       </div>
       <div className='p-6 pt-0'>
         <button className='select-none rounded-lg bg-blue-500 py-3 px-6 text-center align-middle font-sans text-sm font-bold uppercase text-white shaddow-md shadow-blue-800/20 transition-all 
-        hover:shadow-blue-800/50'>Ir a GitHub</button>
+        hover:shadow-blue-800/50' >Ir a GitHub</button>
       </div>
     </div>
   )
